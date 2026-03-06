c7876632684ba61585b43cede4a824b3f2e65d64
vinayak <vcsishere@gmail.com>
2026-03-06 13:48:39 +0530

    Warning message

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print ("this is my first commit")
+
+print("as a collabarator now i am able push the changes directly to master")
+
+print("however ths is not a ideal scenario, because if every change is pushed to master without proper governance results into pool quality master branch")
