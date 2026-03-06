af4a167cfc4d54d4ca80be460d4f56c1375d8fa1
vinayak <vcsishere@gmail.com>
2026-03-06 14:20:01 +0530

    With PR process in place

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print ("this is my first commit")
 print("as a collabarator now i am able push the changes directly to master")
 
 print("however ths is not a ideal scenario, because if every change is pushed to master without proper governance results into pool quality master branch")
+print("test")
